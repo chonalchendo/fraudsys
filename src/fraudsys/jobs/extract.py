a5e3afab170f78c9737bc3d3891334d8962c35ad
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-05-25 21:42:42 +0100

    feat(extract): added part to split labels from production dataset

--- src/fraudsys/jobs/extract.py
+++ src/fraudsys/jobs/extract.py
@@ -12,16 +12,20 @@ class ExtractJob(base.DataJob):
     KIND: T.Literal["extract"] = "extract"
 
     input: datasets.LoaderKind = pdt.Field(..., discriminator="KIND")
+
     output_training_df: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
     output_production_df: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
+    output_production_labels_df: datasets.WriterKind = pdt.Field(
+        ..., discriminator="KIND"
+    )
+
     sensitive_columns: list[str]
+    target_column: str
 
     @T.override
     def run(self) -> base.Locals:
-        # self.logger_service.start()
         logger = self.logger_service.logger()
 
-        # load data from kaggle
         logger.info("Loading data from Kaggle...")
         data = self.input.load()
 
@@ -33,24 +37,24 @@ class ExtractJob(base.DataJob):
 
         train, test = data
 
-        # merge train and test dataset
         logger.info("Merging train and test datasets...")
         merged_df = self._merge_datasets(train, test)
 
-        # generate customer ids
         logger.info("Generating customer IDs...")
         merged_dff = self._generate_ids(merged_df)
 
-        # split datasets again
         logger.info("Splitting datasets...")
         train_df, test_df = self._split_datasets(merged_dff)
 
-        # write out dataset with customer IDs
+        logger.info("Separating labels from production data...")
+        prod_df, labels_df = self._get_labels(test_df)
+
         logger.info("Writing out training data...")
         self.output_training_df.write(train_df)
 
         logger.info("Writing out production data...")
-        self.output_production_df.write(test_df)
+        self.output_production_df.write(prod_df)
+        self.output_production_labels_df.write(labels_df)
 
         return locals()
 
@@ -91,3 +95,8 @@ class ExtractJob(base.DataJob):
                 test_df.with_columns(pl.lit("test").alias("original_split")),
             ]
         )
+
+    def _get_labels(self, prod_df: pl.DataFrame) -> tuple[pl.DataFrame, pl.DataFrame]:
+        is_fraud_df = prod_df.select(self.target_column)
+        features_df = prod_df.drop(self.target_column)
+        return features_df, is_fraud_df
