21d75272547b88a1ca5b36763b93fad0e818e1c3
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-06-01 21:19:55 +0100

    feat(jobs): added experiment and offline features jobs to init file

--- src/fraudsys/jobs/__init__.py
+++ src/fraudsys/jobs/__init__.py
@@ -1,3 +1,5 @@
+from .experiment import ExperimentJob
 from .extract import ExtractJob
+from .offline_features import OfflineFeaturesJob
 
-JobKind = ExtractJob
+JobKind = ExtractJob | ExperimentJob | OfflineFeaturesJob
