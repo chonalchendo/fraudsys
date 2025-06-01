9095f7ce8fca5a0705f8151fc58fe32fc3d424b9
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-06-01 21:23:18 +0100

    feat(pipelines): added function to create model pipeline used in each sklearn model

---
+++ src/fraudsys/core/pipelines.py
@@ -0,0 +1,35 @@
+import typing as T
+
+from imblearn import over_sampling, pipeline, under_sampling
+from sklearn import compose, preprocessing
+
+if T.TYPE_CHECKING:
+    from fraudsys.core import models
+
+
+def create_pipeline(
+    model: "models.ModelKind",
+    numerical_columns: list[str],
+    category_columns: list[str],
+    random_state: int,
+) -> pipeline.Pipeline:
+    # subcomponents
+    categoricals_transformer = preprocessing.OneHotEncoder(
+        sparse_output=False, handle_unknown="ignore"
+    )
+    # components
+    transformer = compose.ColumnTransformer(
+        [
+            ("categoricals", categoricals_transformer, category_columns),
+            ("numericals", "passthrough", numerical_columns),
+        ],
+        remainder="drop",
+    )
+    # pipeline
+    return pipeline.Pipeline(
+        steps=[
+            ("transformer", transformer),
+            ("smote", over_sampling.SMOTE(random_state=random_state)),
+            ("model", model),
+        ]
+    )
