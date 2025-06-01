c6667ca988b22c7ce25203a5c98ea3a27693deb7
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-06-01 21:14:22 +0100

    refactor(extract): refactored the extract job to load out datasets in a better format

--- src/fraudsys/jobs/extract.py
+++ src/fraudsys/jobs/extract.py
@@ -4,6 +4,7 @@ from hashlib import sha256
 import polars as pl
 import pydantic as pdt
 
+from fraudsys import constants
 from fraudsys.io import datasets
 from fraudsys.jobs import base
 
@@ -13,18 +14,16 @@ class ExtractJob(base.DataJob):
 
     input: datasets.LoaderKind = pdt.Field(..., discriminator="KIND")
 
-    output_training_df: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
-    output_production_df: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
-    output_production_labels_df: datasets.WriterKind = pdt.Field(
+    output_inputs_train: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
+    output_targets_train: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
+    output_inputs_production: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
+    output_targets_production: datasets.WriterKind = pdt.Field(
         ..., discriminator="KIND"
     )
 
-    sensitive_columns: list[str]
-    target_column: str
-
     @T.override
     def run(self) -> base.Locals:
-        logger = self.logger_service.logger()
+        logger = self.logger.logger()
 
         logger.info("Loading data from Kaggle...")
         data = self.input.load()
@@ -44,17 +43,21 @@ class ExtractJob(base.DataJob):
         merged_dff = self._generate_ids(merged_df)
 
         logger.info("Splitting datasets...")
-        train_df, test_df = self._split_datasets(merged_dff)
+        train_df, prod_df = self._split_datasets(merged_dff)
+
+        logger.info("Separating labels from training data...")
+        inputs_train, targets_train = self._get_labels(train_df)
 
         logger.info("Separating labels from production data...")
-        prod_df, labels_df = self._get_labels(test_df)
+        inputs_prod, targets_prod = self._get_labels(prod_df)
 
         logger.info("Writing out training data...")
-        self.output_training_df.write(train_df)
+        self.output_inputs_train.write(inputs_train)
+        self.output_targets_train.write(targets_train)
 
         logger.info("Writing out production data...")
-        self.output_production_df.write(prod_df)
-        self.output_production_labels_df.write(labels_df)
+        self.output_inputs_production.write(inputs_prod)
+        self.output_targets_production.write(targets_prod)
 
         return locals()
 
@@ -70,7 +73,7 @@ class ExtractJob(base.DataJob):
             return f"CUST_{sha256(identifier.encode()).hexdigest()[:16]}"
 
         return merged_df.with_columns(
-            pl.struct(self.sensitive_columns)
+            pl.struct(constants.SENSITIVE_COLUMNS)
             .map_elements(lambda x: _create_customer_id(x), return_dtype=pl.String)
             .alias("customer_id")
         )
@@ -97,6 +100,8 @@ class ExtractJob(base.DataJob):
         )
 
     def _get_labels(self, prod_df: pl.DataFrame) -> tuple[pl.DataFrame, pl.DataFrame]:
-        is_fraud_df = prod_df.select(self.target_column)
-        features_df = prod_df.drop(self.target_column)
+        is_fraud_df = prod_df.select(constants.TARGET_COLUMN).with_row_index(
+            name="instant"
+        )
+        features_df = prod_df.drop(constants.TARGET_COLUMN)
         return features_df, is_fraud_df
