7742c9a6da47c0aea70032b6e239cfd8f2dae10a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-06-02 21:00:40 +0100

    feat(jobs): added tuning job to jobs init file

--- src/fraudsys/jobs/__init__.py
+++ src/fraudsys/jobs/__init__.py
@@ -1,5 +1,6 @@
 from .experiment import ExperimentJob
 from .extract import ExtractJob
 from .offline_features import OfflineFeaturesJob
+from .tuning import TuningJob
 
-JobKind = ExtractJob | ExperimentJob | OfflineFeaturesJob
+JobKind = ExtractJob | ExperimentJob | OfflineFeaturesJob | TuningJob
