82135f9666e87bf7cabdeff9dff0740f68bfd495
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-06-24 21:42:17 +0100

    feat(services): include MonitoringService in ServiceKind and __all__ exports

--- src/fraudsys/services/__init__.py
+++ src/fraudsys/services/__init__.py
@@ -1,7 +1,8 @@
 from .api.main import APIService
 from .feature import FeatureService
+from .monitoring import MonitoringService
 from .producer import ProducerService
 
-ServiceKind = FeatureService | ProducerService | APIService
+ServiceKind = FeatureService | ProducerService | APIService | MonitoringService
 
-__all__ = ["FeatureService", "ProducerService", "APIService"]
+__all__ = ["FeatureService", "ProducerService", "APIService", "MonitoringService"]
