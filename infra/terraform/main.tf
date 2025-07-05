4f7354252940741e1e0287df3637ad729a3aa8f4
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-07-05 14:30:49 +0100

    feat(terraform): update IAM and S3 outputs for project user management

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,18 +1,30 @@
 locals {
+  # Project user for all AWS resources
+  project_user_name = var.project_name
+  
+  # S3 bucket names
   mlflow_bucket_name = "${var.project_name}-${var.environment}-mlflow-artifacts"
   feast_bucket_name  = "${var.project_name}-${var.environment}-feast-offline-store"
-  mlflow_user_name   = "fraudsys"
 }
 
 module "s3" {
-  source             = "./modules/s3"
+  source = "./modules/s3"
+  
+  project_name = var.project_name
+  environment  = var.environment
+  
   mlflow_bucket_name = local.mlflow_bucket_name
   feast_bucket_name  = local.feast_bucket_name
 }
 
 module "iam" {
-  source             = "./modules/iam"
-  mlflow_user_name   = local.mlflow_user_name
-  mlflow_bucket_name = local.mlflow_bucket_name
-  mlflow_bucket_arn  = module.s3.mlflow_bucket_arn
+  source = "./modules/iam"
+  
+  project_name      = var.project_name
+  environment       = var.environment
+  project_user_name = local.project_user_name
+  
+  # Bucket ARNs from S3 module
+  mlflow_bucket_arn = module.s3.mlflow_bucket_arn
+  feast_bucket_arn  = module.s3.feast_bucket_arn
 }
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,10 +1,25 @@
-output "mlflow_access_key_id" {
-  description = "Access key ID for MLflow user"
-  value       = module.iam.mlflow_access_key_id
+output "project_access_key_id" {
+  description = "Access key ID for project user"
+  value       = module.iam.project_access_key_id
 }
 
-output "mlflow_secret_access_key" {
-  description = "Secret access key for MLflow user"
-  value       = module.iam.mlflow_secret_access_key
+output "project_secret_access_key" {
+  description = "Secret access key for project user"
+  value       = module.iam.project_secret_access_key
   sensitive   = true
+}
+
+output "project_user_name" {
+  description = "Name of the project user"
+  value       = module.iam.project_user_name
+}
+
+output "mlflow_bucket_name" {
+  description = "Name of the MLflow bucket"
+  value       = module.s3.mlflow_bucket_name
+}
+
+output "feast_bucket_name" {
+  description = "Name of the Feast bucket"
+  value       = module.s3.feast_bucket_name
 }
