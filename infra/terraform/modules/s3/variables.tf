899161ab77c8eca7dcfdbb0274729ba22bcff497
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-07-05 14:30:55 +0100

    feat(terraform): update descriptions for MLflow and Feast bucket variables

--- infra/terraform/modules/s3/variables.tf
+++ infra/terraform/modules/s3/variables.tf
@@ -1,11 +1,19 @@
+variable "project_name" {
+  type        = string
+  description = "The project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (dev, staging, prod)"
+}
+
 variable "mlflow_bucket_name" {
   type        = string
-  description = "Mlflow artifact bucket."
-  default     = "mlflow-artifacts"
+  description = "MLflow artifact bucket name"
 }
 
 variable "feast_bucket_name" {
   type        = string
-  description = "Feast offline store bucket."
-  default     = "feast-offline-store"
+  description = "Feast offline store bucket name"
 }
