16329caceaa31055477ce26c792ed1b6be1efce1
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-07-16 19:26:07 +0100

    feat(dependencies): move context and producer functions to a new dependencies module

--- src/fraudsys/services/api/app.py
+++ src/fraudsys/services/api/app.py
@@ -1,63 +1,10 @@
-from functools import lru_cache
-
-import mlflow
 import polars as pl
 from fastapi import Depends, FastAPI
 
 from fraudsys.core import features, schemas
-from fraudsys.io import kafka, registries
+from fraudsys.io import kafka
+from fraudsys.services.api import dependencies as deps
 from fraudsys.services.api import models as api_models
-
-_context: api_models.AppContext | None = None
-
-
-def inject_context(ctx: api_models.AppContext):
-    global _context
-    _context = ctx
-
-
-def get_context() -> api_models.AppContext:
-    if not _context:
-        raise ValueError("_context is None")
-    return _context
-
-
-@lru_cache
-def get_kafka_producer() -> kafka.KafkaProducerWrapper:
-    ctx: api_models.AppContext = get_context()
-    return kafka.KafkaProducerWrapper(
-        topic=ctx.raw_transactions_topic, servers=ctx.kafka_servers
-    )
-
-
-@lru_cache
-def get_predictions_producer() -> kafka.KafkaProducerWrapper:
-    ctx: api_models.AppContext = get_context()
-    return kafka.KafkaProducerWrapper(
-        topic=ctx.predictions_topic, servers=ctx.kafka_servers
-    )
-
-
-@lru_cache
-def get_model() -> registries.CustomReader.Adapter:
-    ctx: api_models.AppContext = get_context()
-    logger = ctx.logger.logger()
-    logger.info(
-        "Reading model",
-        tracking_server=ctx.mlflow_tracking_uri,
-        registry=ctx.mlflow_registry,
-        alias=ctx.mlflow_model_alias,
-    )
-    # Set the tracking and registry URIs
-    mlflow.set_tracking_uri(ctx.mlflow_tracking_uri)
-    mlflow.set_registry_uri(ctx.mlflow_tracking_uri)  # same as tracking for local
-
-    model_uri = registries.uri_for_model_alias_or_version(
-        name=ctx.mlflow_registry, alias_or_version=ctx.mlflow_model_alias
-    )
-    model = registries.CustomReader().read(uri=model_uri)
-    return model
-
 
 app = FastAPI()
 
@@ -70,8 +17,8 @@ def health() -> dict[str, str]:
 @app.post("/predict", response_model=api_models.InferenceResponse)
 async def submit_transaction(
     trxn: api_models.RawTransaction,
-    producer: kafka.KafkaProducerWrapper = Depends(get_kafka_producer),
-    preds_producer: kafka.KafkaProducerWrapper = Depends(get_predictions_producer),
+    producer: kafka.KafkaProducerWrapper = Depends(deps.get_kafka_producer),
+    preds_producer: kafka.KafkaProducerWrapper = Depends(deps.get_predictions_producer),
 ) -> api_models.InferenceResponse:
     trxn_message = trxn.model_dump()
 
@@ -85,7 +32,7 @@ async def submit_transaction(
     input_ = cleaned_input.to_pandas()
     input = schemas.InputsSchema.check(input_)
 
-    model = get_model()
+    model = deps.get_model()
     output = model.predict(inputs=input)
     pred = output["prediction"].iloc[0]
 
